6f51d3c4731f40a05e5248a65444d6b2fc1d65ca
Joselaf <luis.jlaf@hotmail.com>
2023-12-13 15:59:48 +0000

    Server connection

--- Client.java
+++ Client.java
@@ -4,5 +4,6 @@ import java.io.*;
 public class Client {
     public static void main(String args[]) throws IOException {
         Socket s = new Socket("localhost", 4999);
+        s.close();
     }
 }
---
+++ Server.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String args[]) throws IOException {
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept();
+
+        System.out.println("Client connected");
+
+        s.close();
+        ss.close();
+    }
+}
