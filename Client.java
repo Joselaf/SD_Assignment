8ac73c12337d4b847c19b39effafeb161f3ee37b
A96296_BG <a96296@alunos.uminho.pt>
2023-12-22 16:24:24 +0000

    registo e autenticação feito

--- Client.java
+++ Client.java
@@ -2,8 +2,33 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
+    private static final String HOST = "localhost";
+    private static final int PORTA = 4999;
     public static void main(String args[]) throws IOException {
-        Socket s = new Socket("localhost", 4999);
-        s.close();
+        try {
+            Socket s = new Socket(HOST, PORTA);
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Escolha a ação: registo ou autenticacao");
+            String acao = consoleInput.readLine();
+            out.println(acao);
+
+            System.out.println("Digite o nome de usuário:");
+            String nomeUsuario = consoleInput.readLine();
+            out.println(nomeUsuario);
+
+            System.out.println("Digite a senha:");
+            String senha = consoleInput.readLine();
+            out.println(senha);
+
+            String resposta = in.readLine();
+            System.out.println(resposta);
+
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Server.java
+++ Server.java
@@ -1,15 +1,89 @@
 import java.net.*;
 import java.io.*;
-import sd23(1).sd
+import java.util.*;
+//import sd23(1).sd
 
 public class Server {
+
+    private static final int PORTA = 4999;
+    private static Map<String, String> usuarios = new HashMap<>();
+
     public static void main(String args[]) throws IOException {
-        ServerSocket ss = new ServerSocket(4999);
-        Socket s = ss.accept();
+        ServerSocket ss = null;
+        try{
+            ss = new ServerSocket(PORTA);
+            System.out.println("Servidor de Autenticação iniciado na porta " + PORTA);
+
+            while (true) {
+                Socket s = ss.accept();
+                new Thread(new AutenticacaoHandler(s)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ss != null && !ss.isClosed()) {
+                    ss.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
         System.out.println("Client connected");
+    }
 
-        s.close();
-        ss.close();
+    private static class AutenticacaoHandler implements Runnable {
+
+        private Socket clienteSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public AutenticacaoHandler(Socket clienteSocket) {
+            this.clienteSocket = clienteSocket;
+        }
+
+        public void run() {
+            try {
+                out = new PrintWriter(clienteSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
+
+                // Receber ação do cliente (registo ou autenticação)
+                String acao = in.readLine();
+
+                if ("registo".equals(acao)) {
+                    registrarUsuario();
+                } else if ("autenticacao".equals(acao)) {
+                    autenticarUsuario();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    clienteSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void registrarUsuario() throws IOException {
+            String nomeUsuario = in.readLine();
+            String senha = in.readLine();
+            usuarios.put(nomeUsuario, senha);
+            out.println("Registo bem-sucedido para o usuário: " + nomeUsuario);
+        }
+
+        private void autenticarUsuario() throws IOException {
+            String nomeUsuario = in.readLine();
+            String senha = in.readLine();
+
+            if (usuarios.containsKey(nomeUsuario) && usuarios.get(nomeUsuario).equals(senha)) {
+                out.println("Autenticação bem-sucedida para o usuário: " + nomeUsuario);
+            } else {
+                out.println("Falha na autenticação. Verifique o nome de usuário e a senha.");
+            }
+        }
     }
 }
